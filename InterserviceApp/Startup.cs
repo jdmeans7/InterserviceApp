5c3632db76227fa2f16b4c254f36d8fe469488e8
Jason Means <jasondmeans7@gmail.com>
2019-02-08 16:06:16 -0500

    Add project files.

---
+++ InterserviceApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InterserviceApp.Startup))]
+namespace InterserviceApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
