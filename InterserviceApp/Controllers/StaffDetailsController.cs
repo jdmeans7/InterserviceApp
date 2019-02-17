9829d4d5738d52370c261f033a84ffa5fe6c6dc8
Jason Means <jasondmeans7@gmail.com>
2019-02-16 19:19:57 -0500

    Models set up and simple CRUD available

---
+++ InterserviceApp/Controllers/StaffDetailsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using InterserviceApp.Models;
+
+namespace InterserviceApp.Controllers
+{
+    public class StaffDetailsController : Controller
+    {
+        private InterserviceModels db = new InterserviceModels();
+
+        // GET: StaffDetails
+        public ActionResult Index()
+        {
+            return View(db.is_staffDetails.ToList());
+        }
+
+        // GET: StaffDetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            is_staffDetails is_staffDetails = db.is_staffDetails.Find(id);
+            if (is_staffDetails == null)
+            {
+                return HttpNotFound();
+            }
+            return View(is_staffDetails);
+        }
+
+        // GET: StaffDetails/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StaffDetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "badgeID,fName,lName,dept,phone,birthdate")] is_staffDetails is_staffDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                db.is_staffDetails.Add(is_staffDetails);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(is_staffDetails);
+        }
+
+        // GET: StaffDetails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            is_staffDetails is_staffDetails = db.is_staffDetails.Find(id);
+            if (is_staffDetails == null)
+            {
+                return HttpNotFound();
+            }
+            return View(is_staffDetails);
+        }
+
+        // POST: StaffDetails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "badgeID,fName,lName,dept,phone,birthdate")] is_staffDetails is_staffDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(is_staffDetails).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(is_staffDetails);
+        }
+
+        // GET: StaffDetails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            is_staffDetails is_staffDetails = db.is_staffDetails.Find(id);
+            if (is_staffDetails == null)
+            {
+                return HttpNotFound();
+            }
+            return View(is_staffDetails);
+        }
+
+        // POST: StaffDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            is_staffDetails is_staffDetails = db.is_staffDetails.Find(id);
+            db.is_staffDetails.Remove(is_staffDetails);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ InterserviceApp/Models/InterserviceModels.cs
@@ -0,0 +1,74 @@
+namespace InterserviceApp.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class InterserviceModels : DbContext
+    {
+        // Your context has been configured to use a 'InterserviceModels' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'InterserviceApp.Models.InterserviceModels' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'InterserviceModels' 
+        // connection string in the application configuration file.
+        public InterserviceModels()
+            : base("name=InterserviceModels")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<is_staffDetails> is_staffDetails { get; set; }
+        public virtual DbSet<Class> Classes { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<StaffClass> StaffClasses { get; set; }
+    }
+
+    public class is_staffDetails
+    {
+        [Key]
+        public int badgeID { get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public string dept { get; set; }
+        public string phone { get; set; }
+        public DateTime birthdate { get; set; }
+    }
+
+    public class Class
+    {
+        [Key]
+        public int classID { get; set; }
+        public DateTime date { get; set; }
+        public string startTime { get; set; }
+        public string room { get; set; }
+        public string capacity { get; set; }
+        public string justification { get; set; }
+        public string fees { get; set; }
+
+        public int courseID { get; set; }
+        public virtual Course Courses { get; set; }
+    }
+
+    public class Course
+    {
+        [Key]
+        public int courseID { get; set; }
+        public string courseCode { get; set; }
+        public string desc { get; set; }
+    }
+
+    public class StaffClass
+    {
+        [Key]
+        public int badgeID { get; set; }
+        public int classID { get; set; }
+        public Boolean approved { get; set; }
+        public DateTime endDate { get; set; }
+        public Boolean status { get; set; }
+    }
+}
