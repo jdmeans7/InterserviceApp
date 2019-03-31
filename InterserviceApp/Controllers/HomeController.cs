e8645bdaa9bac68ee1d42dc00973decf4201eddc
Jason Means <jasondmeans7@gmail.com>
2019-03-30 22:08:26 -0400

    Home page created for showing required classes and then all classes

--- InterserviceApp/Controllers/HomeController.cs
+++ InterserviceApp/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using InterserviceApp.Models;
+using System.Data.Entity;
 
 namespace InterserviceApp.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return View();
@@ -30,6 +34,33 @@ namespace InterserviceApp.Controllers
         public ActionResult ClassPortal()
         {
             return View();
+        }
+
+        public ActionResult HomeRegister()
+        {
+            /*
+            var result = (from Class in db.Classes
+                          join Course in db.Courses
+                          on Class.courseID equals Course.courseID
+                          select new
+                          {
+                              classID = Class.classID
+                          }).ToList();
+            List<int> ids = new List<int>();
+            foreach(var item in result)
+            {
+                ids.Add(item.classID);
+            }
+            ViewBag.Req = ids;
+            */
+            var classes = db.Classes.Include(i => i.Course).Where(a => a.Course.required == true);
+            return View(classes);
+        }
+
+        [ChildActionOnly]
+        public ActionResult Classes()
+        {
+            return PartialView(db.Classes.Include(i => i.Course).ToList());
         }
     }
 }
